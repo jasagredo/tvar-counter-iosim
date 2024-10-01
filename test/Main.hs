5163203506ece7f364cd64dcf2f7a7b3b94cd710
Javier Sagredo <jasataco@gmail.com>
2024-10-01 12:28:57 +0200

    Extend with IOSim

--- test/Main.hs
+++ test/Main.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Main (main) where
 
 import Control.Concurrent.Class.MonadSTM.Strict
 import Control.Monad.Class.MonadAsync
 import Control.Monad.Class.MonadTest
+import Control.Monad.Class.MonadTimer
 import Control.Monad.IOSim
+import Data.Bifunctor
 import Test.QuickCheck
+import Test.Tasty
+import Test.Tasty.QuickCheck
 
 data AtomicCounter s = AtomicCounter (StrictTVar (IOSim s) Int)
 
@@ -14,9 +20,13 @@ newCounter = do
     atomically $ labelTVar ref "TheCounter"
     return (AtomicCounter ref)
 
-incr :: AtomicCounter s -> IOSim s ()
-incr (AtomicCounter ref) = do
+incr :: Maybe (Int, Int) -> AtomicCounter s -> IOSim s ()
+incr mDelays (AtomicCounter ref) = do
+    -- Introduce delays to test with IOSim
+    maybe (pure ()) (threadDelay . fst) mDelays
     i <- readTVarIO ref
+    -- Introduce delays to test with IOSim
+    maybe (pure ()) (threadDelay . snd) mDelays
     atomically $ writeTVar ref (i + 1)
 
 get :: AtomicCounter s -> IOSim s Int
@@ -24,17 +34,28 @@ get (AtomicCounter ref) = readTVarIO ref
 
 main :: IO ()
 main =
-    quickCheck $
-        exploreSimTrace
-            (\opts -> opts{explorationDebugLevel = 1})
-            ( do
-                exploreRaces
-                counter <- newCounter
-                _ <- concurrently (incr counter) (incr counter)
-                get counter
-            )
-            ( \_ tr ->
-                case traceResult False tr of
-                    Left err -> counterexample (show err) $ property False
-                    Right tw -> property $ tw === 2
-            )
+    defaultMain $
+        testGroup
+            "Broken counter"
+            [ testProperty "IOSimPOR finds race" $
+                expectFailure $
+                    exploreSimTrace
+                        (\opts -> opts{explorationDebugLevel = 1})
+                        (testCase Nothing Nothing)
+                        ( \_ tr ->
+                            case traceResult False tr of
+                                Left err -> counterexample (show err) $ property False
+                                Right tw -> tw === 2
+                        )
+            , testProperty "IOSim finds race" $ \x ->
+                2 === runSimOrThrow (uncurry testCase . bimap Just Just $ x)
+            , testProperty "IOSim also finds ok case" $ \x ->
+                2 =/= runSimOrThrow (uncurry testCase . bimap Just Just $ x)
+            ]
+  where
+    testCase :: forall s. Maybe (Int, Int) -> Maybe (Int, Int) -> IOSim s Int
+    testCase delays1 delays2 = do
+        exploreRaces
+        counter <- newCounter
+        _ <- concurrently (incr delays1 counter) (incr delays2 counter)
+        get counter
